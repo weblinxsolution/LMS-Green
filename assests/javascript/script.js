ec25561e41c4abca510cf8de13a3bdd0af73bbe1
weblinxsolution <103132259+weblinxsolution@users.noreply.github.com>
2023-10-25 20:05:03 +0500

    two new page added

---
+++ assests/javascript/script.js
@@ -0,0 +1,22 @@
+const video = document.getElementById("myVideo");
+const playPauseButton = document.getElementById("playPauseButton");
+
+playPauseButton.addEventListener("click", function () {
+    if (video.paused) {
+        video.play();
+        playPauseButton.innerHTML = '<i class="ri-pause-circle-line"></i>'; // Change button to pause icon
+    } else {
+        video.pause();
+        playPauseButton.innerHTML = '<i class="ri-play-circle-fill"></i>'; // Change button to play icon
+    }
+});
+
+video.addEventListener("click", function () {
+    if (video.paused) {
+        video.play();
+        playPauseButton.innerHTML = '<i class="ri-pause-circle-line"></i>'; // Change button to pause icon
+    } else {
+        video.pause();
+        playPauseButton.innerHTML = '<i class="ri-play-circle-fill"></i>'; // Change button to play icon
+    }
+});
