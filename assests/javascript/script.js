4139f1c367366ec9699dc8d44e65b660e438a755
weblinxsolution <103132259+weblinxsolution@users.noreply.github.com>
2023-10-26 20:38:43 +0500

    Title Change

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,6 +1,9 @@
 const video = document.getElementById("myVideo");
 const playPauseButton = document.getElementById("playPauseButton");
 
+
+
+
 playPauseButton.addEventListener("click", function () {
     if (video.paused) {
         video.play();
